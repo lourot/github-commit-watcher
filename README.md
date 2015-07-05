faf556637d031b72b205f4f41ae6b5c7ecb44bc4
Aurelien Lourot <aurelien.lourot@gmail.com>
2015-07-05 11:39:01 +0200

    watchlist command implemented.

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 
 GitHub's *Watch* feature doesn't send notifications when commits are pushed. This script aims to
 implement this feature and much more.
+
+```bash
+# sudo pip install PyGithub
+$ ./gicowa.py watchlist AurelienLourot
+AurelienLourot/uncommitted
+AurelienLourot/crouton-emacs-conf
+brillout/FasterWeb
+AurelienLourot/github-commit-watcher
+```
--- gicowa.py
+++ gicowa.py
@@ -2,18 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import github
 
 def _watchlist(args):
-    print "watchlist"
+    hub = github.Github()
+    for repo in hub.get_user(args.username).get_subscriptions():
+        print repo.full_name
 
 def _main():
     parser = argparse.ArgumentParser(description="watch GitHub commits easily")
     subparsers = parser.add_subparsers(help="available commands")
 
-    descr = "list projects watched by a user"
+    descr = "list repos watched by a user"
     parser_watchlist = subparsers.add_parser("watchlist", description=descr, help=descr)
     parser_watchlist.set_defaults(impl=_watchlist)
-    parser_watchlist.add_argument("username", help="watcher's name")
+    parser_watchlist.add_argument("username", help="watcher's name (e.g. 'AurelienLourot')")
 
     args = parser.parse_args()
     args.impl(args)
