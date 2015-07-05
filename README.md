30fb21971f026f0fdab5dd5ba3408db977f87985
Aurelien Lourot <aurelien.lourot@gmail.com>
2015-07-05 13:19:34 +0200

    lastrepocommits command implemented.

--- README.md
+++ README.md
@@ -7,9 +7,15 @@ implement this feature and much more.
 
 ```
 # sudo pip install PyGithub
+
 $ ./gicowa.py watchlist AurelienLourot
 AurelienLourot/uncommitted
 AurelienLourot/crouton-emacs-conf
 brillout/FasterWeb
 AurelienLourot/github-commit-watcher
+
+$ ./gicowa.py lastrepocommits AurelienLourot/github-commit-watcher 2015 07 05 09 12 00
+2015-07-05 10:46:27 - Aurelien Lourot - Minor cleanup.
+2015-07-05 09:39:01 - Aurelien Lourot - watchlist command implemented.
+2015-07-05 09:12:00 - Aurelien Lourot - argparse added.
 ```
--- gicowa.py
+++ gicowa.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import datetime
 import github
 
 def _main():
@@ -13,6 +14,20 @@ def _main():
     parser_watchlist.set_defaults(impl=_watchlist)
     parser_watchlist.add_argument("username", help="watcher's name (e.g. 'AurelienLourot')")
 
+    descr = "list last commits of a repo"
+    parser_lastrepocommits = subparsers.add_parser("lastrepocommits", description=descr, help=descr)
+    parser_lastrepocommits.set_defaults(impl=_lastrepocommits)
+    parser_lastrepocommits.add_argument("repo",
+        help="repository's full name (e.g. 'AurelienLourot/github-commit-watcher')")
+    since = parser_lastrepocommits.add_argument_group("since",
+        "oldest committer UTC timestamp to consider (e.g. '2015 07 05 09 12 00')")
+    since.add_argument("YYYY", help="year")
+    since.add_argument("MM", help="month")
+    since.add_argument("DD", help="day")
+    since.add_argument("hh", help="hour")
+    since.add_argument("mm", help="minute")
+    since.add_argument("ss", help="second")
+
     args = parser.parse_args()
     args.impl(github.Github(), args)
 
@@ -22,6 +37,18 @@ def _watchlist(github, args):
     """
     for repo in github.get_user(args.username).get_subscriptions():
         print repo.full_name
+
+def _lastrepocommits(github, args):
+    """Implements 'lastrepocommits' command.
+       Prints all commits of 'args.repo' with committer timestamp bigger than
+       'args.YYYY,MM,DD,hh,mm,ss'.
+    """
+    since = datetime.datetime(int(args.YYYY), int(args.MM), int(args.DD), int(args.hh),
+                              int(args.mm), int(args.ss))
+    repo = github.get_repo(args.repo)
+    for i in repo.get_commits(since=since):
+        commit = repo.get_git_commit(i.sha)
+        print "%s - %s - %s" % (commit.committer.date, commit.committer.name, commit.message)
 
 if __name__ == "__main__":
     _main()
